47db186aebaebcdf65175c54e98b8a3c89d3fd3d
carsonshoupe <38569953+carsonshoupe@users.noreply.github.com>
2018-08-31 09:02:15 -0400

    The assignment

---
+++ CarsonStack.java
@@ -0,0 +1,23 @@
+public class CarsonStack {
+  CarsonStack() { #TODO: }
+  
+  public void push(int a) {
+  	#TODO: 
+  }
+  
+  public int pop() {
+    #TODO:
+  }
+}
+
+CarsonStack s = new CarsonStack()
+  
+s.push(1)
+s.push(2)
+s.push(3)
+  
+a = s.pop() #3
+b = s.pop() #2
+c = s.pop() #1
+
+Last In, First Out
